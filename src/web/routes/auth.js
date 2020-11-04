ce8633cb0f003df9966c72d14715b2a1c6a045ed
Rene <r.vleug@gmail.com>
2020-11-04 21:48:32 +0100

    add login route and redirect

--- src/web/routes/auth.js
+++ src/web/routes/auth.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 
 router.get('/login', function(req, res, next) {
     let client = require('./../../main.js');
+    if(req.session.loggedIn) return res.redirect('/guilds');
     res.render('layouts/master', { body: 'auth/login', header: 'empty', link: client.config.oauth.oauthURL });
 });
 
